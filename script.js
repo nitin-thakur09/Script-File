ec8a94931b70473974de49b5a7e691913b6e0114
nitin-thakur09 <nitinthakur12332112@gmail.com>
2024-08-04 11:08:24 +0530

    Question 4

--- script.js
+++ script.js
@@ -26,21 +26,39 @@
 
 // Question 3
 
+// {
+//     let car = {
+//     companyName: 'Honda',
+//     model: 'Amaze',
+//     year: 2020
+// };
+
+// function changeCarYear(nyear) {
+//     car.year = nyear;
+// }
+
+// changeCarYear(2024);
+
+// const { model, year } = car;
+
+// console.log(`Model: ${model}`);
+// console.log(`Year: ${year}`);
+// }
+
+// Question 4
+
 {
-    let car = {
-    companyName: 'Honda',
-    model: 'Amaze',
-    year: 2020
-};
-
-function changeCarYear(nyear) {
-    car.year = nyear;
-}
-
-changeCarYear(2024);
-
-const { model, year } = car;
-
-console.log(`Model: ${model}`);
-console.log(`Year: ${year}`);
+    function isPrime(num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        if (num % 2 === 0 || num % 3 === 0) return false;
+        for (let i = 5; i * i <= num; i += 6) {
+            if (num % i === 0 || num % (i + 2) === 0) return false;
+        }
+    
+        return true;
+    }
+    const numbers = [2, 3, 4, 5, 10, 13, 17, 18, 19, 23, 25];
+    const primes = numbers.filter(isPrime);
+    console.log(primes);
 }
