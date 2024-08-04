621605bc2ac0a60f57a0dac94eae35f797e90c9d
nitin-thakur09 <nitinthakur12332112@gmail.com>
2024-08-04 11:04:41 +0530

    Question 2

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 // Question 1
 
+// {
+//     let array=[1,2,3,4];
+//     array.map((x)=>{
+//         x=x**2;
+//         console.log(x);
+//     })
+// }
+
+
+// Question 2
+
 {
-    let array=[1,2,3,4];
-    array.map((x)=>{
-        x=x**2;
-        console.log(x);
-    })
+    let grade=stdscr(Number(prompt()));
+    function stdscr(value){
+        console.log(value);
+    if(value>80&&value<=100){return "A"}
+    else if(value>60&&value<=80){return "B"}
+    else if(value>50&&value<=60){return "C"}
+    else if(value>33&&value<=50){return "D"}
+    else if(value>=0&&value<=33){return "F"}
+    else{return "Invalide"}
+}
 }
